68689a131287c388928cd617b37b4bff9ffd7080
allang <allang4779@gmail.com>
2021-12-21 13:09:04 +0300

    Initial commit to the repo

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "build.js",
+  },
+
+  module: {
+    //   loaders
+    rules: [
+      { test: /\.js|jsx$/, exclude: /node_modules/, use:{
+          loader:"babel-loader"
+      } },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/public/index.html"),
+      filename: "index.html",
+      title: "React app",
+      
+    }),
+  ],
+};
